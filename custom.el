9f93ad6d88bd8d12ca73c5e8c298ebbc4046da2b
wxjeacen <wxjeacen@gmail.com>
2012-11-10 13:19:20 +0800

    add new color-theme

--- custom.el
+++ custom.el
@@ -15,8 +15,11 @@
     )
   )
 
-(my-require-package 'color-theme)
-(color-theme-classic)
+;;(my-require-package 'color-theme)
+;;(color-theme-initialize)
+;;(color-theme-robin-hood)
+(load-theme 'solarized-dark)
+
 (my-require-package 'json)
 (my-require-package 'erc)
 (my-require-package 'emacs-jabber)
@@ -61,3 +64,15 @@
     (byte-recompile-directory (expand-file-name "~/.emacs.d/site-lisps") 0)
     )
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
