bbf60e3e67c0fc5cd9b2dbe81121480d003f1e2e
wxjeacen <wxjeacen@gmail.com>
2012-11-29 21:13:19 +0800

    add erc-use-proxy option

--- my-lisps/erc-settings.el
+++ my-lisps/erc-settings.el
@@ -5,7 +5,10 @@
 ;; -*- Emacs-Lisp -*-
 
 (require 'socks)
-(setq erc-server-connect-function 'socks-open-network-stream)
-(setq socks-server (list "My socks server" "127.0.0.1" 6650 5))
 
+(defun erc-use-proxy ()
+  (interactive)
+  (setq erc-server-connect-function 'socks-open-network-stream)
+  (setq socks-server (list "My socks server" "127.0.0.1" 6650 5))
+)
 (provide 'erc-settings)
